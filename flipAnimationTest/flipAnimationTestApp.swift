b9062b89054b0ae8057245bf9b5dd7954924a8b5
Dong487 <Dsheng7777777@gmail.com>
2022-09-13 17:00:46 +0800

    Initial Commit

---
+++ flipAnimationTest/flipAnimationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  flipAnimationTestApp.swift
+//  flipAnimationTest
+//
+//  Created by DONG SHENG on 2022/9/13.
+//
+
+import SwiftUI
+
+@main
+struct flipAnimationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
